2b3a5a60ca49ce5a2ceac316487021b52f0c9cf2
sasho33 <sasho3333333@gmail.com>
2023-12-09 18:21:51 +0000

    creadentials configured

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
-/* 
+/*
  * Package Imports
-*/
+ */
 
-const path = require("path");
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const partials = require('express-partials');
 
-
 const app = express();
-
 
 /*
  * Variable Declarations
-*/
+ */
 
 const PORT = 3000;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
+console.log(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
 /*
  * Passport Configurations
-*/
-
-
-
-
-
-
+ */
 
 /*
  *  Express Project Setup
-*/
+ */
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -39,16 +31,13 @@ app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-
-
-
 /*
  * Routes
-*/
+ */
 
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
-})
+});
 
 app.get('/account', (req, res) => {
   res.render('account', { user: req.user });
@@ -56,23 +45,19 @@ app.get('/account', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.render('login', { user: req.user });
-})
+});
 
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
 
-
-
-
 /*
  * Listener
-*/
+ */
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 /*
  * ensureAuthenticated Callback Function
-*/
-
+ */
